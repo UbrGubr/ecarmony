d5b75fead511f3db213729df0f2f124cd93d86be
matthewscroggs <mattatt14@yahoo.com>
2015-11-16 22:56:43 -0800

    Added a basic ListView for the Driver Profile Page.
    Changes made to the DriverProfileFragment.java and the driver_profile_layout.xml

--- app/src/main/java/me/firstandroidapp/DriverProfileFragment.java
+++ app/src/main/java/me/firstandroidapp/DriverProfileFragment.java
@@ -6,13 +6,16 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by pjjimiso on 10/20/2015.
  */
-public class DriverProfileFragment extends android.support.v4.app.Fragment {
+public class DriverProfileFragment extends android.support.v4.app.ListFragment {
     private String title;
     private int page;
+
+
 
     public static DriverProfileFragment newInstance(int page, String title){
         DriverProfileFragment driverFragment = new DriverProfileFragment();
@@ -33,9 +36,20 @@ public class DriverProfileFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.driver_profile_layout, container, false);
-        //TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
-        //tvLabel.setText(page + " -- " + title);
-        //return inflater.inflate(R.layout.passenger_profile_layout, container, false);
-        return view;
+
+        View rootView = inflater.inflate(R.layout.driver_profile_layout, container,
+                false);
+
+        String[] values = new String[] {
+                "To: 10/17, 3 riders, 10:00 AM","To: 10/18, 1 rider, 11:30 AM",
+                "To: 10/19, 3 riders, 10:00 AM","To: 10/20, 1 rider, 11:30 AM",
+                "From: 10/17, 3 riders, 3:00 PM","From: 10/18, 2 riders, 5:30 PM",
+                "From: 10/19, 3 riders, 3:00 PM","From: 10/20, 2 riders, 5:30 PM"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+        return rootView;
+        //return view;
     }
 }
