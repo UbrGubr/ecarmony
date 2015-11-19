e5eb5d1e3fc47187e63bd476effd3a76253cc443
UbrGubr <pat.jimison@gmail.com>
2015-11-19 00:44:59 -0800

    Added the HeaderTextArrayAdapter type array which holds either a <Header> or a <ListItem> object.

--- app/src/main/java/me/firstandroidapp/DriverProfileFragment.java
+++ app/src/main/java/me/firstandroidapp/DriverProfileFragment.java
@@ -7,6 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import me.firstandroidapp.HeaderTextArrayAdapter.Item;
+import me.firstandroidapp.HeaderTextArrayAdapter.Header;
+import me.firstandroidapp.HeaderTextArrayAdapter.ListItem;
+
 
 /**
  * Created by pjjimiso on 10/20/2015.
@@ -35,18 +41,24 @@ public class DriverProfileFragment extends android.support.v4.app.ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.driver_profile_layout, container,
+        View view = inflater.inflate(R.layout.driver_profile_layout, container,
                 false);
 
-        String[] values = new String[] {
-                "To: 10/17, 3 riders, 10:00 AM","To: 10/18, 1 rider, 11:30 AM",
-                "To: 10/19, 3 riders, 10:00 AM","To: 10/20, 1 rider, 11:30 AM",
-                "From: 10/17, 3 riders, 3:00 PM","From: 10/18, 2 riders, 5:30 PM",
-                "From: 10/19, 3 riders, 3:00 PM","From: 10/20, 2 riders, 5:30 PM"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.text_view_layout,R.id.text_view, values);
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Header("To"));
+        items.add(new ListItem("10/17 - 10:00AM", "3 riders"));
+        items.add(new ListItem("10/18 - 11:30AM", "1 rider"));
+        items.add(new ListItem("10/19 - 10:00AM", "3 riders"));
+        items.add(new ListItem("10/20 - 11:30AM", "1 rider"));
+        items.add(new Header("From"));
+        items.add(new ListItem("10/17 - 3:00PM", "3 riders"));
+        items.add(new ListItem("10/18 - 5:30PM", "2 riders"));
+        items.add(new ListItem("10/19 - 3:00PM", "3 riders"));
+        items.add(new ListItem("10/20 - 5:30PM", "2 riders"));
+
+        HeaderTextArrayAdapter adapter = new HeaderTextArrayAdapter(getActivity(), items);
         setListAdapter(adapter);
-        return rootView;
+
+        return view;
     }
 }
