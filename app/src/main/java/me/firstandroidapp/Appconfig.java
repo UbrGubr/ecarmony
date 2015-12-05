904e72d8d2845ef3a09e2d8e4cfce3ff21934c34
UbrGubr <pat.jimison@gmail.com>
2015-12-05 09:58:11 -0800

    Changes prepping for php mysql queries

---
+++ app/src/main/java/me/firstandroidapp/Appconfig.java
@@ -0,0 +1,12 @@
+package me.firstandroidapp;
+
+/**
+ * Created by PJ on 12/5/2015.
+ */
+public class Appconfig {
+    // Server login url
+    public static String URL_LOGIN = "http://130.86.67.252/~jimisonp/login_register_api/";
+
+    // Server register url
+    public static String URL_REGISTER = "http://130.86.67.252/~jimisonp/login_register_api/";
+}
